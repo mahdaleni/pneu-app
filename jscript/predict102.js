aaee2453ca3cb583006537a84a06ba0e666b1278
Arif <dewisandrax@gmail.com>
2020-01-29 11:02:19 +0700

    Add files via upload

---
+++ jscript/predict102.js
@@ -0,0 +1,113 @@
+
+
+function simulateClick(tabID) {
+	
+	document.getElementById(tabID).click();
+}
+
+function predictOnLoad() {
+	
+	// Simulate a click on the predict button
+	setTimeout(simulateClick.bind(null,'predict-button'), 500);
+};
+
+
+$("#image-selector").change(function () {
+	let reader = new FileReader();
+	reader.onload = function () {
+		let dataURL = reader.result;
+		$("#selected-image").attr("src", dataURL);
+		$("#prediction-list").empty();
+	}
+	
+		
+		let file = $("#image-selector").prop('files')[0];
+		reader.readAsDataURL(file);
+		
+		
+		
+		setTimeout(simulateClick.bind(null,'predict-button'), 500);
+
+});
+
+
+
+
+let model;
+(async function () {
+	
+	model = await tf.loadModel('https://pneumonia-app.now.sh/model_kaggle_1/model.json');
+	$("#selected-image").attr("src", "https://pneumonia-app.now.sh/assets/normal.jpeg")
+	
+	
+	
+	// Hide the model loading spinner
+	$('.progress-bar').hide();
+	
+	// Simulate a click on the predict button
+	predictOnLoad();
+	
+	
+})();
+
+
+
+
+
+
+$("#predict-button").click(async function () {
+	
+	
+	
+	let image = $('#selected-image').get(0);
+	
+	// Pre-process the image
+	let tensor = tf.fromPixels(image)
+	.resizeNearestNeighbor([224,224])
+	.toFloat();
+	
+	
+	let offset = tf.scalar(127.5);
+	
+	tensor = tensor.sub(offset)
+	.div(offset)
+	.expandDims();
+	
+	
+	
+	
+	
+	let predictions = await model.predict(tensor).data();
+	let top5 = Array.from(predictions)
+		.map(function (p, i) { // this is Array.map
+			return {
+				probability: p,
+				className: TARGET_CLASSES[i] // we are selecting the value from the obj
+			};
+				
+			
+		}).sort(function (a, b) {
+			return b.probability - a.probability;
+				
+		}).slice(0, 3);
+	
+	
+$("#prediction-list").empty();
+top5.forEach(function (p) {
+
+	$("#prediction-list").append(`<li>${p.className}: ${p.probability.toFixed(6)}</li>`);
+
+	
+	});
+	
+	
+});
+
+
+
+
+
+
+
+
+
