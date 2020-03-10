10312658e440fe6d346dc09c3e67b1a339f17d8d
Arif <dewisandrax@gmail.com>
2020-03-10 10:15:08 +0700

    Update predict102.js

--- jscript/predict102.js
+++ jscript/predict102.js
@@ -89,7 +89,7 @@ $("#predict-button").click(async function () {
 		}).sort(function (a, b) {
 			return b.probability - a.probability;
 				
-		}).slice(0, 3);
+		}).slice(0, 2);
 	
 	
 $("#prediction-list").empty();
